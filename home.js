4883f44baf31910ac4b063fe2ce630d195b36694
Kenn <kakomea@yahoo.com>
2020-06-11 20:14:26 +0000

    fix unresponsive divs

--- home.js
+++ home.js
@@ -1,12 +1,12 @@
-$(function() {
+(function() {
 var selectedClass = "";
-$(".filter").click(function(){
+(".filter").click(function(){
 selectedClass = $(this).attr("data-rel");
-$("#gallery").fadeTo(100, 0.1);
-$("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+("#gallery").fadeTo(100, 0.1);
+("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
 setTimeout(function() {
-$("."+selectedClass).fadeIn().addClass('animation');
-$("#gallery").fadeTo(300, 1);
+("."+selectedClass).fadeIn().addClass('animation');
+("#gallery").fadeTo(300, 1);
 }, 300);
 });
 });
