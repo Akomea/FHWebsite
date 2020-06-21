921b250c11e08d98b81908b0521a8d91400b157c
Kenn <kakomea@yahoo.com>
2020-06-21 07:45:39 +0000

    custom css for touch targets

--- js/index.js
+++ js/index.js
@@ -1,14 +1,28 @@
-(function() {
-var selectedClass = "";
-(".filter").click(function(){
-selectedClass = $(this).attr("data-rel");
-("#gallery").fadeTo(100, 0.1);
-("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-setTimeout(function() {
-("."+selectedClass).fadeIn().addClass('animation');
-("#gallery").fadeTo(300, 1);
-}, 300);
-});
+(document).ready(function(){
+
+    (".filter").click(function(){
+        var value = (this).attr('data-rel');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            ('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            (".filter").not('.'+value).hide('3000');
+            ('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ((".filter").removeClass("active")) {
+(this).removeClass("active");
+}
+(this).addClass("active");
+
 });
 
 
